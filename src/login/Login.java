ed5e21843e690a896473386d9b683295802fc2e5
96655 <96655@LAPTOP-R8L8KB7J>
2024-03-09 17:13:17 +0300

    AmjadBajaber amjadbaja1424@gmail.com

---
+++ src/login/Login.java
@@ -0,0 +1,19 @@
+
+package login;
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      String user_name;
+      String password;
+      
+        System.out.println("Enter user name: ");
+        user_name = input.nextLine();
+        System.out.println("Enter password: ");
+        password = input.nextLine();
+    }
+    
+}
